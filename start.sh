817d8aa70ad3eaf7531797ee550b9c12ee159a06
dcdc8888 <rph322@gmail.com>
2018-08-28 01:17:40 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 sysctl net.ipv4.ip_forward=1
 iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE
 
+rm -f /var/run/starter.charon.pid
+
 echo ": PSK \"$VPN_PSK\"" >/etc/ipsec.secrets
 echo "$VPN_USER : XAUTH \"$VPN_PASS\"" >>/etc/ipsec.secrets
 
