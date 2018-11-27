e95deae1dc247f8d574fc6b168c2dea3695d4b27
dcdc8888 <rph322@gmail.com>
2018-11-27 22:37:04 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 sysctl net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 rm -f /var/run/starter.charon.pid
 
