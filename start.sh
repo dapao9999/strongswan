133dabd02f1228d19526e339b3b7afab9649c12c
dcdc8888 <rph322@gmail.com>
2019-12-29 00:07:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 sysctl net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -j MASQUERADE
-
+iptables -t nat -A POSTROUTING -o eth0 -m policy --dir out --pol ipsec -j ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 rm -f /var/run/starter.charon.pid
-
 echo ": PSK ${PSK:-vpn}" >/etc/ipsec.secrets
-echo "${USER:-vpn} : XAUTH ${PASS:-vpn}" >>/etc/ipsec.secrets
-
 ipsec start --nofork 
