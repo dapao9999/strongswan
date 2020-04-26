390218f5f8c209d86d25fb63c0aac15389892e92
root <root@DESKTOP-SSVSGP1.localdomain>
2020-04-26 11:05:34 +0800

    add ikev1

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 sysctl net.ipv4.ip_forward=1
 iptables -t nat -A POSTROUTING -j MASQUERADE
+
 rm -f /var/run/starter.charon.pid
+
 echo ": PSK ${PSK:-vpn}" >/etc/ipsec.secrets
+echo "${USER:-vpn} : XAUTH ${PASSWORD:-vpn}">>/etc/ipsec.secrets
+
 ipsec start --nofork 
